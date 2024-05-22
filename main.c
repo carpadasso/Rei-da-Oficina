b025c68dcd82fd9e29d8bf668f92b5d8e343a3e9
Leonardo Carpwiski <lionheartcarpwiski@gmail.com>
2024-05-21 21:00:03 -0300

    KOW 0.3

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <allegro5/allegro_acodec.h>
 #include "player.h"
 
+// DEFINES
 #define TRUE 1
 #define FALSE 0
 
@@ -19,12 +20,13 @@ int main(int argc, char* argv[])
    ALLEGRO_EVENT event;
 
    // Inits necessários
-   al_init();
-   al_init_font_addon();
-   al_init_image_addon();
-   al_install_keyboard();
-   al_install_audio();
-   al_init_acodec_addon();
+   al_init();                 // Carregamento da biblioteca padrão Allegro
+   al_init_font_addon();      // Carregamento da biblioteca de fontes
+   al_init_ttf_addon();       // Carregamento da biblioteca de fontes .ttf
+   al_init_image_addon();     // Carregamento da biblioteca de imagens
+   al_install_keyboard();     // Carregamento da biblioteca do teclado
+   al_install_audio();        // Carregamento da biblioteca de áudio
+   al_init_acodec_addon();    // Carregamento da biblioteca de formatos de áudio
 
    // Criação dos elementos do jogo
    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
@@ -49,8 +51,9 @@ int main(int argc, char* argv[])
    ---------------------------------------------------------------------------*/
    // Imagens do Menu
    ALLEGRO_BITMAP* logo_menu = al_load_bitmap("./assets/logo_menu.png");
+   ALLEGRO_BITMAP* arrow_select = al_load_bitmap("./assets/arrow_select_menu.png");
    // Fonte usada no Menu
-   ALLEGRO_FONT* font = al_load_ttf_font("./assets/font/upheaval.ttf", 14, 0);
+   ALLEGRO_FONT* font = al_load_ttf_font("./assets/font/CooperBits.ttf", 24, 0);
    // Música do Menu
    al_reserve_samples(1);
    ALLEGRO_SAMPLE* menu_music = al_load_sample("./assets/music/menu.mp3");
@@ -58,48 +61,125 @@ int main(int argc, char* argv[])
    al_play_sample(menu_music, 0.25, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, &menu_music_id);
 
    unsigned short transp = 0;
+   unsigned short selected_option = 0;
    while (TRUE)
    {
       al_wait_for_event(event_queue, &event);
 
+      // Clicar no X -> Fecha o jogo
       if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
-         al_destroy_bitmap(logo_menu);
-         al_destroy_bitmap(icon_display);
-         al_destroy_font(font);
-         al_destroy_display(display);
+         // Elementos Gerais
          al_destroy_timer(timer);
          al_destroy_event_queue(event_queue);
+         al_destroy_display(display);
+         // Elementos Menu
+         al_destroy_font(font);
+         al_destroy_bitmap(icon_display);
+         al_destroy_sample(menu_music);
+         // Imagens Menu
+         al_destroy_bitmap(logo_menu);
+         al_destroy_bitmap(arrow_select);
          return 0;
       }
-
-      if (event.keyboard.keycode == ALLEGRO_KEY_C) break;
-
-      al_clear_to_color(al_map_rgb(0, 0, 0));
-      al_draw_scaled_bitmap(logo_menu, 0, 0, 1000, 560, 400 - (1000 / 2) / 2, 50, (1000 / 2), (560 / 2), 0);
-
-      if (transp < 255) transp += 4;
-      if (transp > 255) transp = 255;
-      al_draw_tinted_scaled_bitmap(logo_menu, al_map_rgb(transp, transp, transp), 0, 0, 1000, 560, 400 - (1000 / 2) / 2, 
-                                   50, (1000 / 2), (560 / 2), 0);
-      //al_draw_text(font, al_map_rgb(255, 255, 255), 600, 400, 0, "Novo Jogo");
-      al_flip_display();
+      // Seleção da Opção
+      if (event.type == ALLEGRO_EVENT_KEY_DOWN){
+         // UP ou DOWN -> Muda de Opção
+         if (event.keyboard.keycode == ALLEGRO_KEY_DOWN || event.keyboard.keycode == ALLEGRO_KEY_UP){
+            if (selected_option == 0) selected_option = 1;
+            else if (selected_option == 1) selected_option = 0;
+         }
+         // ENTER -> Seleciona a Opção
+         else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER){
+            if (selected_option == 0) break; // Avança para a Tela de Seleção
+            else if (selected_option == 1){  // Fecha o jogo
+               // Elementos Gerais
+               al_destroy_timer(timer);
+               al_destroy_event_queue(event_queue);
+               al_destroy_display(display);
+               al_destroy_font(font);
+               // Elementos Menu
+               al_destroy_bitmap(icon_display);
+               al_destroy_sample(menu_music);
+               // Imagens Menu
+               al_destroy_bitmap(logo_menu);
+               al_destroy_bitmap(arrow_select);
+               return 0;
+            }
+         }
+      }
+      // CLOCK TIMER -> Exibição de Elementos Gráficos do Menu
+      else if (event.type == ALLEGRO_EVENT_TIMER){
+         al_clear_to_color(al_map_rgb(0, 0, 0));                                  // Fundo da Tela
+         al_draw_tinted_scaled_bitmap(logo_menu, al_map_rgb(255, 255, 255), 0, 
+                                      0, 1000, 560, 400 - (1000 / 2) / 2,50, 
+                                      (1000 / 2), (560 / 2), 0);                  // Logo do Street Fighter
+         al_draw_bitmap(arrow_select, 280, 360 + 50 * selected_option, 0);        // Seta de Escolha de Opção
+         al_draw_text(font, al_map_rgb(255, 255, 255), 330, 360, 0, "Novo Jogo"); // Opção 1
+         al_draw_text(font, al_map_rgb(255, 255, 255), 330, 410, 0, "Sair");      // Opção 2
+         al_flip_display();
+      }
    }
-
    al_stop_sample(&menu_music_id); // Interrompe a música do Menu
+   al_destroy_bitmap(logo_menu);
+   al_destroy_bitmap(arrow_select);
+   al_destroy_bitmap(icon_display);
+   al_destroy_sample(menu_music);
+
+   /* --------------------------------------------------------------------------
+   *  TELA DE SELEÇÃO
+   ---------------------------------------------------------------------------*/
+   unsigned short character_select_opt = 0;
+   while (TRUE)
+   {
+      al_wait_for_event(event_queue, &event);
+
+      if (event.type == ALLEGRO_EVENT_TIMER){
+         al_clear_to_color(al_map_rgb(34, 35, 36));
+         al_draw_text(font, al_map_rgb(255, 255, 255), 150, 100, 0, "Modo de Luta: ");
+         if (character_select_opt == 0)
+            al_draw_text(font, al_map_rgb(255, 255, 255), 370, 100, 0, "Ryu vs Ken");
+         else if (character_select_opt == 1)
+            al_draw_text(font, al_map_rgb(255, 255, 255), 370, 100, 0, "Ken vs Ryu");
+         al_flip_display();
+      }
+      else if (event.type == ALLEGRO_EVENT_KEY_DOWN){
+         if (event.keyboard.keycode == ALLEGRO_KEY_LEFT || event.keyboard.keycode == ALLEGRO_KEY_RIGHT){
+            if (character_select_opt == 0) character_select_opt = 1;
+            else if (character_select_opt == 1) character_select_opt = 0;
+         }
+         else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) break;
+      }
+      else if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+         // Elementos Gerais
+         al_destroy_timer(timer);
+         al_destroy_event_queue(event_queue);
+         al_destroy_display(display);
+         al_destroy_font(font);
+         return 0;
+      }
+   }
 
    /* --------------------------------------------------------------------------
    *  CONFIGURAÇÃO DA LUTA
    ---------------------------------------------------------------------------*/
-   ALLEGRO_BITMAP* personagem = al_load_bitmap("./assets/characters/ryu/idle.png");
+   ALLEGRO_BITMAP* p1_idle = al_load_bitmap("./assets/characters/ryu/idle.png");
+   ALLEGRO_BITMAP* p1_walk_pos = al_load_bitmap("./assets/characters/ryu/walk_pos.png");
+   ALLEGRO_BITMAP* p1_walk_neg = al_load_bitmap("./assets/characters/ryu/walk_neg.png");
    ALLEGRO_BITMAP* fundo = al_load_bitmap("./assets/background/one_piece.jpg");
-   int pos_x = 100, pos_y = 200;
-   int frame = 0;
+   unsigned short pos_x = 100, pos_y = 200;
+   unsigned short idle_frame = 0, walk_frame = 0;
+   unsigned short position = 0, flag_position = 0;
+   int tecla;
+
    while (TRUE)
    {
       al_wait_for_event(event_queue, &event);
 
       // Clicar no X -> Fecha o jogo
       if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) break;
+
+      // Exibe o Cenário de Fundo
+      al_draw_bitmap_region(fundo, 960, 400, 800, 600, 0, 0, 0);
 
       // Tecla ESC -> Pausa o jogo
       if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE){
@@ -110,37 +190,59 @@ int main(int argc, char* argv[])
          continue;
       }
 
-      if (pos_x >= 0 && pos_x <= DISPLAY_WIDTH && pos_y >= 0 && pos_y <= DISPLAY_HEIGHT){
-         if (event.keyboard.keycode == ALLEGRO_KEY_A)
-            pos_x -= 5;
-         if (event.keyboard.keycode == ALLEGRO_KEY_D)
+      // Calcula a posição do jogador com relação ao meio da tela
+      if (pos_x - 70 < DISPLAY_WIDTH / 2) position = 0;
+      else position = 1;
+      // Calcula se deve exibir o sprite invertido ou não
+      if (position == 0) flag_position = 0;
+      else if (position == 1) flag_position = ALLEGRO_FLIP_HORIZONTAL;
+      
+      /* Aperto de Tecla */
+      // D -> MOVIMENTA para a DIREITA
+      // A -> MOVIMENTA para a ESQUERDA
+      // W -> PULA
+      // S -> AGACHA
+      // Z -> ATAQUE com MEMBROS SUPERIORES
+      // X -> ATAQUE com MEMBROS INFERIORES
+      if (event.type == ALLEGRO_EVENT_KEY_DOWN){
+         tecla = event.keyboard.keycode;
+         if (tecla == ALLEGRO_KEY_D){
             pos_x += 5;
-         if (event.keyboard.keycode == ALLEGRO_KEY_W)
-            pos_y -= 5;
-         if (event.keyboard.keycode == ALLEGRO_KEY_S)
-            pos_y += 5;
-      } else {
-         pos_x = 1;
-         pos_y = 1;
+            //walk_frame += 1;
+            //if (walk_frame > 24) walk_frame = 1;
+            //al_draw_scaled_bitmap(p1_walk_pos, 70 * (walk_frame / 6), 0, 70, 95, pos_x, pos_y, 140, 190, flag_position);
+         }
+         if (tecla == ALLEGRO_KEY_A){
+            pos_x -= 3;
+            //walk_frame += 1;
+            //if (walk_frame > 24) walk_frame = 1;
+            //al_draw_scaled_bitmap(p1_walk_neg, 70 * (walk_frame / 6), 0, 70, 95, pos_x, pos_y, 140, 190, flag_position);
+         }
+         if (tecla == ALLEGRO_KEY_W) pos_y -= 5;
+         if (tecla == ALLEGRO_KEY_S) pos_y += 5;
       }
-
-      frame += 1;
-      if (frame > 10) frame = 1;
-
-      al_clear_to_color(al_map_rgb(255, 255, 255));
-      al_draw_bitmap_region(fundo, 960, 400, 800, 600, 0, 0, 0);
-      if (pos_x <= DISPLAY_WIDTH / 2)
-         //al_draw_bitmap_region(personagem, 70 * (frame / 2), 0, 70, 95, pos_x, pos_y, 0);
-         al_draw_scaled_bitmap(personagem, 70 * (frame / 2), 0, 70, 95, pos_x, pos_y, 140, 190, 0);
-      else
-         //al_draw_bitmap_region(personagem, 70 * (frame / 2), 0, 70, 95, pos_x, pos_y, ALLEGRO_FLIP_HORIZONTAL);
-         al_draw_scaled_bitmap(personagem, 70 * (frame / 2), 0, 70, 95, pos_x, pos_y, 140, 190, ALLEGRO_FLIP_HORIZONTAL);
+      /* Soltura de Tecla */
+      // A ou D -> CANCELA a animação de movimento
+      if (event.type == ALLEGRO_EVENT_KEY_UP){
+         tecla = event.keyboard.keycode;
+         if (tecla == ALLEGRO_KEY_D) { walk_frame = 0; idle_frame = 0; }
+         else if (tecla == ALLEGRO_KEY_A) { walk_frame = 0; idle_frame = 0; }
+      }
+      /* Batida de Relógio */
+      // EXECUTA a animação de PARADO do personagem
+      if (event.type == ALLEGRO_EVENT_TIMER){
+         idle_frame += 1;
+         if (idle_frame > 20) idle_frame = 1;
+         al_draw_scaled_bitmap(p1_idle, 70 * (idle_frame / 5), 0, 70, 95, pos_x, pos_y, 140, 190, flag_position);
+      }
       al_flip_display();
    }
 
    // Destruição de todos os bitmaps
    al_destroy_bitmap(icon_display);
-   al_destroy_bitmap(personagem);
+   al_destroy_bitmap(p1_idle);
+   al_destroy_bitmap(p1_walk_pos);
+   al_destroy_bitmap(p1_walk_neg);
    al_destroy_bitmap(fundo);
 
    // Destruição dos elementos do jogo
--- pisca.c
+++ pisca.c
@@ -4,42 +4,54 @@
 
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 
 #define TRUE 1
 #define FALSE 0
+#define DISPLAY_WIDTH  800
+#define DISPLAY_HEIGHT 600
 
 int main(int argc, char* argv[])
 {
    al_init();
    al_init_font_addon();
+   al_init_ttf_addon();
 
-   ALLEGRO_TIMER* timer = al_create_timer(1.0 / 2000.0);
+   ALLEGRO_TIMER* timer = al_create_timer(1.0 / 120.0);
    ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
-   ALLEGRO_FONT* font = al_create_builtin_font();
-   ALLEGRO_DISPLAY* display = al_create_display(800, 600);
+   ALLEGRO_FONT* font = al_load_ttf_font("./assets/font/CooperBits.ttf", 32, 0);
+   ALLEGRO_DISPLAY* display = al_create_display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
    
    al_register_event_source(event_queue, al_get_display_event_source(display));
    al_register_event_source(event_queue, al_get_timer_event_source(timer));
    
    al_start_timer(timer);
    ALLEGRO_EVENT event;
+   ALLEGRO_TRANSFORM transf;
+   al_identity_transform(&transf);
 
-   unsigned short ida = TRUE;
-   unsigned short cor = 0;
-
-   while (TRUE){
+   unsigned short largura_string = al_get_text_width(font, "STREET FIGHTER");
+   unsigned short altura_string = al_get_font_line_height(font);
+   unsigned short largura, altura;
+   unsigned short print_mov = 0, movement = 0;
+   while (TRUE)
+   {
       al_wait_for_event(event_queue, &event);
+
       if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) break;
+      else if (event.type == ALLEGRO_EVENT_TIMER){
+         if (print_mov > 1) print_mov = 0;
+         else print_mov++;
 
-      if (event.type == ALLEGRO_EVENT_TIMER){
-         if (ida) cor++;
-         else cor--;
-
-         if (cor == 0) ida = TRUE;
-         if (cor == 255) ida = FALSE;
-
-         al_clear_to_color(al_map_rgb(cor, cor, cor));
+         al_clear_to_color(al_map_rgb(34, 35, 36));
+         for (altura = 0; altura < 2* DISPLAY_HEIGHT; altura += altura_string)
+            for (largura = 0; largura < 2 * DISPLAY_WIDTH; largura += largura_string + 10)
+               al_draw_text(font, al_map_rgb(77, 78, 79), -50 + largura, -400 + altura + 5 * (movement / 5), 0, "STREET FIGHTER ");
          al_flip_display();
+      }
+      if (print_mov == 1){
+         if (movement <= 50) movement += 5;
+         else movement = 0;
       }
    }
 
